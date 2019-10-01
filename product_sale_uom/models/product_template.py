de372f584889334ecf7d91749919f99b8a8972f7
Gabriela Rivero <regaby@gmail.com>
2019-10-01 10:20:01 -0300

    [UPD] Sale oum stock button

--- product_sale_uom/models/product_template.py
+++ product_sale_uom/models/product_template.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models, _
+from odoo.addons import decimal_precision as dp
+from odoo.tools.float_utils import float_round
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
@@ -11,5 +13,18 @@ class ProductTemplate(models.Model):
         'product.uom', 'Sale Unit of Measure',
         default=_get_default_uom_id, required=True,
         help="Default Unit of Measure used for sale orders. It must be in the same category than the default unit of measure.")
+
+    uom_sale_qty_available = fields.Float(
+        string='Quantity On Hand (2Unit)',
+        compute='_compute_uom_sale_qty_available',
+        digits=dp.get_precision('Product Unit of Measure'),
+    )
+
+    def _compute_uom_sale_qty_available(self):
+        for product in self.filtered('uom_sale_id'):
+            qty = product.qty_available / (
+                product.uom_sale_id.factor_inv or 1.0)
+            product.uom_sale_qty_available = float_round(
+                qty, precision_rounding=product.uom_id.rounding)
 
 
