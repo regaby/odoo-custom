24a1179d3d5929f8030bc3380151d3fe2acf3b23
Gabriela Rivero <regaby@gmail.com>
2020-09-15 21:18:10 -0300

    [ADD] module l10n_ar_pos_moldeo_fix

---
+++ l10n_ar_pos_moldeo_fix/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'POS einvoice ticket fix',
+    'version': '0.1',
+    'author': 'Ing. Gabriela Rivero',
+    'license': 'LGPL-3',
+    'category': 'Point Of Sale',
+    'website': 'Localizacion arg.',
+    'depends': ['l10n_ar_pos_einvoice_ticket'],
+    'summary': "Módulo de parche para impresión de ticket de FE con la localización de moldeo v13.0",
+    'data': [
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+}
---
+++ l10n_ar_pos_moldeo_fix/models/pos_order.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+
+class pos_order(models.Model):
+    _inherit = "pos.order"
+
+    def action_pos_order_invoice(self):
+        moves = self.env['account.move']
+
+        for order in self:
+            # Force company for all SUPERUSER_ID action
+            if order.account_move:
+                moves += order.account_move
+                continue
+
+            if not order.partner_id:
+                raise UserError(_('Please provide a partner for the sale.'))
+
+            move_vals = order._prepare_invoice_vals()
+            new_move = moves.sudo()\
+                            .with_context(default_type=move_vals['type'], force_company=order.company_id.id)\
+                            .create(move_vals)
+            message = _("This invoice has been created from the point of sale session: <a href=# data-oe-model=pos.order data-oe-id=%d>%s</a>") % (order.id, order.name)
+            new_move.message_post(body=message)
+            order.write({'account_move': new_move.id, 'state': 'invoiced'})
+            new_move.sudo().with_context(force_company=order.company_id.id).action_post()
+            moves += new_move
+
+        if not moves:
+            return {}
+
+        return {
+            'name': _('Customer Invoice'),
+            'view_mode': 'form',
+            'view_id': self.env.ref('account.view_move_form').id,
+            'res_model': 'account.move',
+            'context': "{'type':'out_invoice'}",
+            'type': 'ir.actions.act_window',
+            'nodestroy': True,
+            'target': 'current',
+            'res_id': moves and moves.ids[0] or False,
+        }
