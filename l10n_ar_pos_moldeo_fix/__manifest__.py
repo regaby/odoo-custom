9f7d8756d4358d542fc3b6b215c5fd5dcec52f09
Gabriela Rivero <regaby@gmail.com>
2020-09-21 13:06:40 -0300

    [UPD] module website info

--- l10n_ar_pos_moldeo_fix/__manifest__.py
+++ l10n_ar_pos_moldeo_fix/__manifest__.py
@@ -5,7 +5,7 @@
     'author': 'Ing. Gabriela Rivero',
     'license': 'LGPL-3',
     'category': 'Point Of Sale',
-    'website': 'Localizacion arg.',
+    'website': 'www.galup.com.ar',
     'depends': ['l10n_ar_pos_einvoice_ticket'],
     'summary': "Módulo de parche para impresión de ticket de FE con la localización de moldeo v13.0",
     'data': [
