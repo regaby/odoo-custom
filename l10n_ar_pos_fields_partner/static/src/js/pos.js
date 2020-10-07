1310a9b7ddfc367498ce7a7c41b89744cbb147d7
Gabriela Rivero <regaby@gmail.com>
2020-10-07 20:26:03 -0300

    [UPD] extiendo save_client_details para el caso de responsable inscripto tome campos como required.

--- l10n_ar_pos_fields_partner/static/src/js/pos.js
+++ l10n_ar_pos_fields_partner/static/src/js/pos.js
@@ -6,6 +6,9 @@ odoo.define('l10n_ar_pos_fields_partner.PosModel', function (require) {
     //var pos_chrome = require('point_of_sale.chrome')
     var models = pos_model.PosModel.prototype.models;
     var PosModelSuper = pos_model.PosModel;
+    var screens = require('point_of_sale.screens');
+    var core = require('web.core');
+    var _t = core._t;
     //var OrderSuper = pos_model.Order;
     //var OrderlineSuper = pos_model.Orderline;
 
@@ -42,6 +45,38 @@ odoo.define('l10n_ar_pos_fields_partner.PosModel', function (require) {
             this.responsability_type = [];
 
         },
+    });
+
+    screens.ClientListScreenWidget.include({
+        save_client_details: function (partner) {
+            this._super(partner);
+            var self = this;
+            var fields = {};
+            this.$('.client-details-contents .detail').each(function(idx,el){
+                if (self.integer_client_details.includes(el.name)){
+                    var parsed_value = parseInt(el.value, 10);
+                    if (isNaN(parsed_value)){
+                        fields[el.name] = false;
+                    }
+                    else{
+                        fields[el.name] = parsed_value
+                    }
+                }
+                else{
+                    fields[el.name] = el.value || false;
+                }
+            });
+            // si es responsable inscripto y tipo doc cuit, vat is required
+            if (!fields.vat && fields.l10n_ar_afip_responsibility_type_id == '1' ) {
+                this.gui.show_popup('error',_t('El campo CUIT (NIF) es requerido.'));
+                return;
+            }
+            if (fields.l10n_latam_identification_type_id != '4' && fields.l10n_ar_afip_responsibility_type_id == '1' ) {
+                this.gui.show_popup('error',_t('Seleccione Tipo Doc. CUIT'));
+                return;
+            }
+        },
+
     });
 
 
