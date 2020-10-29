bb9a35f95a6a4727283ef4d5e563de5ff8d36a6a
Gabriela Rivero <regaby@gmail.com>
2020-10-29 04:05:44 -0300

    [ADD] pos_disable_pdf_invoice_download module

---
+++ pos_disable_pdf_invoice_download/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'POS Disable PDF Invoice Download',
+    'version': '0.1',
+    'author': 'Ing. Gabriela Rivero',
+    'license': 'LGPL-3',
+    'category': 'Point Of Sale',
+    'website': 'www.galup.com.ar',
+    'summary': 'This module allows optional invoice printing from POS',
+    'depends': ['point_of_sale'],
+    'data': [
+        'views/pos_disable_download.xml',
+        'views/pos_config.xml',
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+}
