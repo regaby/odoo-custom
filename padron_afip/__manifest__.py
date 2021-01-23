eadf295bf1ccf15ea6e64e3e4b04b8ca51528643
Gabriela Rivero <regaby@gmail.com>
2021-01-23 16:20:09 -0300

    [ADD] padron_afip module

---
+++ padron_afip/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Padron Afip',
+    'version': '0.1',
+    'author': 'Localizacion arg.',
+    'license': 'LGPL-3',
+    'category': 'Localization/Argentina',
+    'website': 'Localizacion arg.',
+    'depends': ['l10n_ar'],
+    'data': [
+        'wizard/res_partner_update_from_padron_wizard_view.xml',
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+}
