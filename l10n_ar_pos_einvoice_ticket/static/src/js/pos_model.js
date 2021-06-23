85319bff3ed83cb32025b798da98e0005f186b68
Gabriela Rivero <regaby@gmail.com>
2021-06-23 17:45:11 -0300

    UPD print test

--- l10n_ar_pos_einvoice_ticket/static/src/js/pos_model.js
+++ l10n_ar_pos_einvoice_ticket/static/src/js/pos_model.js
@@ -219,7 +219,40 @@ odoo.define('l10n_ar_pos_einvoice_ticket', function (require) {
                 this._super();
             }
         },
-        // handle_auto_print: function() {
+        print: function() {
+            var self = this;
+    
+            if (!this.pos.proxy.printer) { // browser (html) printing
+    
+                // The problem is that in chrome the print() is asynchronous and doesn't
+                // execute until all rpc are finished. So it conflicts with the rpc used
+                // to send the orders to the backend, and the user is able to go to the next 
+                // screen before the printing dialog is opened. The problem is that what's 
+                // printed is whatever is in the page when the dialog is opened and not when it's called,
+                // and so you end up printing the product list instead of the receipt... 
+                //
+                // Fixing this would need a re-architecturing
+                // of the code to postpone sending of orders after printing.
+                //
+                // But since the print dialog also blocks the other asynchronous calls, the
+                // button enabling in the setTimeout() is blocked until the printing dialog is 
+                // closed. But the timeout has to be big enough or else it doesn't work
+                // 1 seconds is the same as the default timeout for sending orders and so the dialog
+                // should have appeared before the timeout... so yeah that's not ultra reliable. 
+    
+                this.lock_screen(true);
+    
+                setTimeout(function(){
+                    self.lock_screen(false);
+                }, 5000);
+    
+                this.print_web();
+            } else {    // proxy (html) printing
+                this.print_html();
+                this.lock_screen(false);
+            }
+        },
+        // handlere_auto_print: function() {
         //     if (this.should_auto_print() && !this.pos.get_order().is_to_email()) {
         //         //this.print();
         //         if (this.should_close_immediately()){
