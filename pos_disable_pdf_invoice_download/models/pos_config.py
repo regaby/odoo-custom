1062976d356a9c187a2fbcfa46799385d995d83f
Gabriela Rivero <regaby@gmail.com>
2023-11-02 10:04:36 -0300

    [UPD][pos_disable_pdf_invoice_download] module ported to 15.0

--- pos_disable_pdf_invoice_download/models/pos_config.py
+++ pos_disable_pdf_invoice_download/models/pos_config.py
@@ -4,6 +4,9 @@ from odoo import api, fields, models, _
 class pos_config(models.Model):
     _inherit = "pos.config"
 
-    print_pdf_invoice = fields.Boolean('Print PDF Invoice', default=1)
+    print_pdf_invoice = fields.Boolean(
+        'Print PDF Invoice', 
+        default=1
+    )
 
 
