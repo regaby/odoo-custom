b432a8902e425d9d974b3de39d0b9196e513462b
Mario Osvaldo Nuñez <marionumza@gmail.com>
2025-05-04 01:03:13 -0300

    Update __manifest__.py

--- l10n_ar_pos_einvoice_ticket/__manifest__.py
+++ l10n_ar_pos_einvoice_ticket/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'POS einvoice ticket',
-    'version': '0.1',
+    'version': '17.0.0.1.0',
     'author': 'Ing. Gabriela Rivero',
     'license': 'LGPL-3',
     'category': 'Point Of Sale',
