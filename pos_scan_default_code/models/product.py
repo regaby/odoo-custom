f2a8219de5963cd0d09d895528e9367e698155c5
Mario Osvaldo Nuñez <marionumza@gmail.com>
2025-05-04 00:02:13 -0300

    Delete pos_scan_default_code directory

--- pos_scan_default_code/models/product.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import api, fields, models, _
-
-class ProductTemplate(models.Model):
-    _inherit = "product.template"
-
-    modelo_articulo = fields.Char('Modelo Artículo')
-
-
