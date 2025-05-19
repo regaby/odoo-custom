2b4c434637cea0a59812272c686700a041da817d
Mario Osvaldo Nuñez <marionumza@gmail.com>
2025-05-19 13:38:19 -0300

    Update pos_session.py

--- l10n_ar_pos_einvoice_ticket_ee/models/pos_session.py
+++ l10n_ar_pos_einvoice_ticket_ee/models/pos_session.py
@@ -17,3 +17,35 @@ class PosSession(models.Model):
             'receipt_invoice_number',
         ]
         return res
+        
+    def _loader_params_res_partner(self):
+        res = super()._loader_params_res_partner()
+        res["search_params"]["fields"] += [
+            'l10n_latam_identification_type_id',
+            'l10n_ar_afip_responsibility_type_id',
+            'city',
+            'state_id',
+            'country_id',
+        ]
+        return res
+        
+    def _pos_ui_models_to_load(self):
+        models = super()._pos_ui_models_to_load()
+        if 'account.tax' not in models:
+            models.append('account.tax')
+        return models
+
+    def _loader_params_account_tax(self):
+        return {
+            'search_params': {
+                'domain': [('company_id', '=', self.config_id.company_id.id)],
+                'fields': [
+                    'name', 
+                    'amount', 
+                    'price_include', 
+                    'include_base_amount', 
+                    'tax_group_id',
+                    'amount_type',
+                ],
+            },
+        }
